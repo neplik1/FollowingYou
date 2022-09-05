666530835717e15eb1cd7d6b42a71cccfd175823
neplik1 <neplik1@gmail.com>
2022-09-05 16:07:29 +0300

    added biding

--- app/src/main/java/com/example/followingyou/LogInFragment.kt
+++ app/src/main/java/com/example/followingyou/LogInFragment.kt
@@ -17,7 +17,7 @@ class LogInFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+        ): View? {
         return inflater.inflate(R.layout.fragment_log_in, container, false)
     }
 
