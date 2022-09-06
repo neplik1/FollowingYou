929e9cf9565ac5101f7b5153fdf4bf0d122b07cf
neplik1 <neplik1@gmail.com>
2022-09-06 17:10:30 +0300

    added LiveData

--- app/src/main/java/com/example/followingyou/presentation/MainActivity.kt
+++ app/src/main/java/com/example/followingyou/presentation/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.followingyou.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.followingyou.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.newsList.observe(this) {
+
+        }
     }
 }
--- app/src/main/java/com/example/followingyou/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/followingyou/presentation/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.followingyou.presentation
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.followingyou.data.NewsListRepositoryImpl
 import com.example.followingyou.domain.DeleteNewsItemUseCase
 import com.example.followingyou.domain.GetNewsListUseCase
+import com.example.followingyou.domain.NewsItem
 
 class MainViewModel : ViewModel() {
     private val repository = NewsListRepositoryImpl
@@ -11,5 +13,10 @@ class MainViewModel : ViewModel() {
     private val getNewsListUseCase = GetNewsListUseCase(repository)
     private val deleteNewsItemUseCase = DeleteNewsItemUseCase(repository)
 
+    val newsList = MutableLiveData<List<NewsItem>>()
 
+    fun getNewsList() {
+        val list = getNewsListUseCase.getNewsList()
+        newsList.value = list
+    }
 }
